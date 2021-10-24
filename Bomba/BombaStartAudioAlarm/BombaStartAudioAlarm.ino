bcf08800ae8fcc556f548c3bdb2a35597491f3ee
appliedpr <applied811@gmail.com>
2021-10-24 09:01:17 -0400

    Signed-off-by: appliedpr <applied811@gmail.com>

---
+++ Bomba/BombaStartAudioAlarm/BombaStartAudioAlarm.ino
@@ -0,0 +1,14 @@
+/*Provides the logic to trigger a sound speech alarm upon
+the water pressure pump activation by monitoring
+the pressure sw contact position to send a signal and 
+start the logic sequence. A WT588d audio ckt is used
+for the speech implementation. Uses ATtiny85 uP.*/
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
