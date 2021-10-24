ccd3184f6608244fcde6bebb7db95ee56b47df7d
appliedpr <applied811@gmail.com>
2021-10-24 10:52:12 -0400

    continued programming ATtiny. Signed-off-by: appliedpr <applied811@gmail.com>

--- Bomba/BombaStartAudioAlarm/BombaStartAudioAlarm.ino
+++ Bomba/BombaStartAudioAlarm/BombaStartAudioAlarm.ino
@@ -3,9 +3,10 @@ the water pressure pump activation by monitoring
 the pressure sw contact position to send a signal and 
 start the logic sequence. A WT588d audio ckt is used
 for the speech implementation. Uses ATtiny85 uP.*/
-void setup() {
-  // put your setup code here, to run once:
 
+void setup() {
+#define pin_pressSwitchStatus 0  // activated=1 deactivated=0 input
+#define pin_Led 1 // indicator LED output
 }
 
 void loop() {
